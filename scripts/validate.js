a0a0922fb7763a61845c8d0969ddf2601bd144ff
Christopher Groskopf <chrisgroskopf@gmail.com>
2025-05-12 15:58:26 -0700

    Add East Hampton, Long Island (id: 31) and id validation script.

---
+++ scripts/validate.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const printsDir = 'src/content/prints';
+
+function run() {
+    const ids = [];
+
+    fs.readdirSync(printsDir).forEach((filename) => {
+        const filePath = path.join(printsDir, filename);
+        const fileContents = fs.readFileSync(filePath, 'utf8');
+        const data = matter(fileContents)['data'];
+
+        if (ids.includes(data.id)) {
+            console.log(`Duplicate id: ${data.id} in ${filename}`);
+        }
+
+        ids.push(data.id);
+    });
+
+    for (let i = 1; i < ids.length + 1; i++) {
+        if (!ids.includes(i)) {
+            console.log(`Missing id: ${i}`);
+        }
+    }
+
+    console.log(`Next id: ${Math.max(...ids) + 1}`)
+}
+
+run();
---
+++ src/content/prints/1880-east-hampton-long-island.md
@@ -0,0 +1,18 @@
+---
+id: 31
+title: East Hampton, L.I.
+image:
+    filename: 1880-east-hampton-long-island.jpg
+    caption: Photo from the Clark Weinreb Collection
+    missing_text: 
+year: "1880"
+size:
+    dimensions: 7 3/16 x 3 1/2 in
+    source: 
+    source_url: 
+signed: In the plate, lower right, interlaced initials S.C. Only known impression also hand-signed.
+publications: []
+drawings: []
+museums: []
+---
+This etching is known from a single impression in the collection of Clark Weinreb. It echoes the subject of one of Colman's earliest etchings, [Old Mill at East Hampton, Long Island](#1877-old-mill-east-hampton-long-island), as well as [Old Mill at Wainscott, Long Island](#1884c-old-mill-wainscott-long-island).
